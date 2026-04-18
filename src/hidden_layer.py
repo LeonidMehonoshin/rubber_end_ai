a5dd82402ee7a5287002c361aa7337c4d2c2c316
LeonidMehonoshin <mehonoshin.leonid1@yandex.ru>
2026-04-18 14:20:18 +0700

    Delete src directory

--- src/hidden_layer.py
+++
@@ -1,9 +0,0 @@
-class Hidden_layer:
-    def __init__(self, np, input_data, weights, biases):
-        self.__np = np
-        self.__input_data = input_data
-        self.__weights = weights
-        self.__biases = biases
-
-    def get(self):
-        return self.__np.tanh(self.__input_data @ self.__weights[0] + self.__biases[0])
--- src/loader.py
+++
@@ -1,8 +0,0 @@
-class Loader:
-    def __init__(self, pickle, path):
-        self.__pickle = pickle
-        self.__path = path
-
-    def get(self):
-        with open(self.__path, 'rb') as file:
-            return self.__pickle.load(file)
