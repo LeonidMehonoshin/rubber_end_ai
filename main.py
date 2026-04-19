eceb394c8b4f464bca325fa3dafdf77c1465ab93
LeonidMehonoshin <mehonoshin.leonid1@yandex.ru>
2026-04-19 17:04:20 +0700

    Add files via upload
    1. Добавлен цветной вывод.
    2. Файл processor.py теперь разделен на два отдельных файла - dataset_transformer.py и 
    input_transformer.py.
    3. Сохранение checkpoint теперь отдельно от trainer.py.
    4. Features теперь сохраняется в checkpoint тоже.
    5. Numpy больше не нужен для работы.

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
-import sys, os
+from color import Color
+print(f'{Color.bold}{Color.green}Init...{Color.end}', end='\r')
+import os
 import yaml
 import torch
 import pandas as pd
-import numpy as np
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+import sklearn
+import time
 
 from model_patcher import ModelPatcher
-from processor import Processor
-from features_helper import FeaturesHelper
+from checkpoint_saver import CheckpointSaver
+from dataset_data_helper import DatasetDataHelper
+from dataset_transformer import DatasetTransformer
+from input_transformer import InputTransformer
 from trainer import Trainer
 from predictor import Predictor
 
 def main():
-    print('Starting...')
     base_path = os.path.dirname(os.path.abspath(__file__))
     config_path = os.path.join(base_path, 'config.yaml')
 
     try:
         with open(config_path, 'r', encoding='utf-8') as f:
             config = yaml.safe_load(f)
-
     except FileNotFoundError:
-        print(f'[ERROR] Config file not found at {config_path}!')
+        print(f'{Color.bold}{Color.red}[ERROR] Config file not found at {config_path}!{Color.end}')
         return
 
     paths = {
@@ -30,61 +32,70 @@ def main():
         'input': os.path.join(base_path, config['paths']['input'])
     }
 
-    training_config = {
-        'max_epochs': config['training']['epochs'],
-        'patience': config['training']['patience'],
-        'trainMode': config['training']['trainMode']
-    }
+    torch.serialization.add_safe_globals([sklearn.preprocessing.LabelEncoder, sklearn.preprocessing.StandardScaler])
+    device = config['training']['device']
 
-    torch.serialization.add_safe_globals([LabelEncoder, StandardScaler, Processor])
-    features_helper = FeaturesHelper()
-    features_list = features_helper.get_features()
-    device = torch.device('cpu')
+    if config['training']['trainMode']:
+        print(f'{Color.yellow}{Color.bold}TrainMode = True!{Color.end}')
 
-    if training_config['trainMode']:
+        for repeat in range(5):
+            for row in [
+                f'{Color.bold}{Color.yellow}    starting!{Color.end}',
+                f'{Color.bold}{Color.red}    STARTING!{Color.end}',
+            ]:
+                print(f'{row} {5-repeat}', end='\r')
+                time.sleep(0.5)
+        print('\r', '             ', '\r', '', end='')
+
         dataset = pd.read_csv(paths['dataset'])
-        processor = Processor(features_list, pd, np, LabelEncoder, StandardScaler)
+        helper = DatasetDataHelper(dataset)
+        features_list = helper.get('features')
         model_patcher = ModelPatcher(len(features_list), torch.nn)
-        Model = model_patcher.get_model_class()
-        model = Model()
-        trainer = Trainer(model, processor, features_helper, torch, paths['checkpoint'])
-        trainer.fit(dataset, training_config['max_epochs'], training_config['patience'])
+        model = model_patcher.get_model_class()()
+
+        trainer = Trainer(
+            model=model,
+            device=device,
+            dataset=dataset,
+            torch=torch,
+            DatasetTransformer=DatasetTransformer,
+            DatasetDataHelper=DatasetDataHelper,
+            pd=pd,
+            sklearn=sklearn,
+            epochs=config['training']['epochs'],
+            patience=config['training']['patience'],
+            Color=Color
+        )
+
+        CheckpointSaver(torch, trainer.get(), paths['checkpoint'], Color)
 
     else:
         try:
-            checkpoint = torch.load(
-                paths['checkpoint'],
-                map_location = device,
-                weights_only = False
-            )
-
-        except FileNotFoundError:
-            print(f'[ERROR] CheckPoint file not found at: {paths['checkpoint']}')
-            return
-
+            checkpoint = torch.load(paths['checkpoint'], map_location = device, weights_only = False)
         except Exception as e:
-            print(f'[ERROR] Failed to load checkpoint: {e}')
+            print(f'{Color.bold}{Color.red}[ERROR] Failed to load checkpoint: {e}{Color.end}')
             return
 
         try:
             with open(paths['input'], 'r', encoding='utf-8') as f:
                 user_input = yaml.safe_load(f)
-
-        except FileNotFoundError:
-            print(f'[ERROR] Input file not found at {paths['input']}')
+        except Exception as e:
+            print(f'{Color.bold}{Color.red}[ERROR] Input error: {e}{Color.end}')
             return
 
-        except yaml.YAMLError:
-            print(f'[ERROR] Failed to decode YAML in {paths['input']}')
-            return
+        features_from_ckpt = checkpoint['processor_state']['features']
+        model_patcher = ModelPatcher(len(features_from_ckpt), torch.nn)
+        model = model_patcher.get_model_class()()
 
-        model_patcher = ModelPatcher(len(checkpoint['features_list']), torch.nn)
-        Model = model_patcher.get_model_class()
-        model = Model()
-        processor = Processor(checkpoint['features_list'], pd, np, LabelEncoder, StandardScaler)
-        predictor = Predictor(checkpoint, processor, torch, model, np, pd, LabelEncoder, StandardScaler)
+        predictor = Predictor(
+            checkpoint=checkpoint,
+            InputTransformer=InputTransformer,
+            model=model,
+            torch=torch,
+            pd=pd
+        )
 
-        result = predictor.get(user_input)
-        print(f'\n[RESULT]: _{result}_ (km)')
+        result = predictor.predict(user_input)
+        print(f'{Color.bold}{Color.cyan}\n[RESULT]: {result} (km){Color.end}')
 
 if __name__ == '__main__': main()
--- predictor.py
+++ predictor.py
@@ -1,16 +1,26 @@
 class Predictor:
-    def __init__(self, checkpoint, processor, torch, model, np, pd, LabelEncoder, StandardScaler):
+    def __init__(self, checkpoint, InputTransformer, model, torch, pd):
         self.__torch = torch
-        self.__checkpoint = checkpoint
-        self.__y_max = self.__checkpoint['y_max']
-        self.__processor = processor
-        self.__processor.set_states(self.__checkpoint['processor_state'])
+        self.__pd = pd
+        self.__target_max = checkpoint['target_max']
         self.__model = model
-        self.__model.load_state_dict(self.__checkpoint['model_state'])
+        self.__model.load_state_dict(checkpoint['model_state'])
         self.__model.eval()
+        self.__processor_state = checkpoint['processor_state']
+        self.__InputTransformer = InputTransformer
 
-    def get(self, user_input):
+    def predict(self, input_data):
         with self.__torch.no_grad():
-            processed_input = self.__torch.tensor(self.__processor.transform_row(user_input)).float()
-            prediction = self.__model(processed_input)
-            return max(0, int(prediction.item() * self.__y_max))
+            transformer = self.__InputTransformer(
+                input_data,
+                self.__processor_state['encoders'],
+                self.__processor_state['scaler'],
+                self.__processor_state['medians'],
+                self.__processor_state['features'],
+                self.__pd
+            )
+
+            processed_tensor = self.__torch.tensor(transformer.get()).float().to(next(self.__model.parameters()).device)
+            output = self.__model(processed_tensor)
+            result = output.item() * self.__target_max
+            return max(0, int(result))
