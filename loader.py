09cb7516e5a19a44831f355c686bf2cc5d6f49c5
LeonidMehonoshin <mehonoshin.leonid1@yandex.ru>
2026-04-23 16:05:30 +0700

    Add files via upload
    улучшен синтаксис файла Loader, добавлено красивое сообщение об ошибке, при не найденом файле dataset.csv

--- loader.py
+++ loader.py
@@ -1,48 +1,51 @@
 class Loader:
-    @staticmethod
-    def load_checkpoint(path, key_path, Fernet, io, torch, Color, device):
+    def __init__(self, yaml, io, torch, Fernet, Color):
+        self.__io = io
+        self.__yaml = yaml
+        self.__torch = torch
+        self.__Color = Color
+        self.__Fernet = Fernet
+
+    def load_checkpoint(self, path, key_path, device):
         try:
             with open(key_path, 'rb') as key_file:
-                key = key_file.read().strip() # Добавь strip на всякий случай
+                key = key_file.read().strip()
 
-            # Читаем именно ЧЕКПОИНТ (path), а не ключ (key_path)
             with open(path, 'rb') as f:
                 encrypted_checkpoint = f.read()
 
-            fernet = Fernet(key)
+            fernet = self.__Fernet(key)
             decrypted_checkpoint = fernet.decrypt(encrypted_checkpoint)
 
-            checkpoint = torch.load(io.BytesIO(decrypted_checkpoint), map_location=device, weights_only=False)
-            return checkpoint # Возвращаем результат
+            checkpoint = self.__torch.load(self.__io.BytesIO(decrypted_checkpoint), map_location = device, weights_only = False)
+            return checkpoint
 
         except FileNotFoundError:
-            print(f'{Color.bold}{Color.red}[ERROR] Key or Checkpoint file not found!{Color.end}')
+            print(f'{self.__Color.bold}{self.__Color.red}[ERROR] Key or Checkpoint file not found!{self.__Color.end}')
             return
 
         except Exception as e:
-            print(f'{Color.bold}{Color.red}[ERROR] Decryption failed: {e}{Color.end}')
+            print(f'{self.__Color.bold}{self.__Color.red}[ERROR] Decryption failed: {e}{self.__Color.end}')
             return
 
-    @staticmethod
-    def load_input(path, yaml, Color):
+    def load_input(self, path):
         try:
-            with open(path, 'r', encoding='utf-8') as f:
-                user_input = yaml.safe_load(f)
+            with open(path, 'r', encoding = 'utf-8') as f:
+                user_input = self.__yaml.safe_load(f)
 
         except Exception as e:
-            print(f'{Color.bold}{Color.red}[ERROR] Input error: {e}{Color.end}')
+            print(f'{self.__Color.bold}{self.__Color.red}[ERROR] Input error: {e}{self.__Color.end}')
             return
 
         return user_input
 
-    @staticmethod
-    def load_config(path, yaml, Color):
+    def load_config(self, path):
         try:
-            with open(path, 'r', encoding='utf-8') as f:
-                config = yaml.safe_load(f)
+            with open(path, 'r', encoding = 'utf-8') as f:
+                config = self.__yaml.safe_load(f)
 
         except FileNotFoundError:
-            print(f'{Color.bold}{Color.red}[ERROR] Config file not found at {path}!{Color.end}')
+            print(f'{self.__Color.bold}{self.__Color.red}[ERROR] Config file not found at {path}!{self.__Color.end}')
             return
 
         return config
